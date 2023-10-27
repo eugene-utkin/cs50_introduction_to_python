bf12c1b1e7d23e88b59652e24ae6027b60d83983
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-28 00:05:55 +0600

    str methods used

--- Lesson 2 - Functions, Variables/hello.py
+++ Lesson 2 - Functions, Variables/hello.py
@@ -1,5 +1,11 @@
 # Ask user for their name
 name = input("What's your name? ")
 
+# Remove whitespace from str
+name = name.strip()
+
+# Capitalize user's name
+name = name.capitalize()
+
 # Say hello to user
-print("hello,", name)
+print(f"hello, {name}")
